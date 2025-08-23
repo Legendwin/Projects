28bb34f23839d8c3d4ad65cca208aa01333d68d3
Legendwin <149319619+Legendwin@users.noreply.github.com>
2025-08-23 23:03:15 +0000

    New projects

---
+++ C++/calculatorApp.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+// Function prototypes
+void show_calc();
+void show_output(double);
+
+int main () {
+    double num1 = 0.0, num2 = 0.0, ans = 0.0;
+    char operation = '\n';
+    bool converter = 0;
+
+    // Taking input from user
+    //show_calc();
+    while (operation != '.') {
+        if (converter == 1) { //To display the output
+            show_output(ans);
+            // Prompt for next operation
+            cout << "\nEnter operation: ";
+            cin >> operation;
+
+            // Taking input from the user based on operation
+            if (operation == '+' || operation == '-' || operation == '*' || operation == '/' || operation == 'm' || operation == 'p') {
+                num1 = ans;
+                cout << "Enter next number: ";
+                cin >> num2;
+            }
+            else if (operation == 'k' || operation == '.') {
+                // Do nothing
+            }
+            else {
+                num1 = ans;
+            }
+        }
+        else { // To display the calculator
+            show_calc();
+            cout << "\nEnter operation: ";
+            cin >> operation;
+
+            // Taking input from the user based on operation
+            if (operation == '+' || operation == '-' || operation == '*' || operation == '/' || operation == 'm' || operation == 'p') {
+                cout << "Enter number: ";
+                cin >> num1;
+                cout << "Enter next number: ";
+                cin >> num2;
+            }
+            else if (operation == 'k' || operation == '.') {
+                // Do nothing
+            }
+            else {
+                cout << "Enter number: ";
+                cin >> num1;
+            }
+
+            converter = 1;
+        }
+
+        switch (operation) {
+            case '+':
+                ans = num1 + num2;
+                break;
+            case '-':
+                ans = num1 - num2;
+                break;
+            case '*':
+                ans = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    cout << "Error: Division by zero is not allowed." << endl;
+                    ans = 0;
+                } 
+                else {
+                    ans = num1 / num2;
+                }
+                break;
+            case 'm':
+                if (num2 == 0) {
+                    cout << "Error: Modulus operation requires non-zero integers." << endl;
+                    ans = 0;
+                } 
+                else {
+                    ans = int(num1) % int(num2);
+                }
+                break;
+            case 'L':
+                if (num1 <= 0) {
+                    cout << "Error: Logarithm of non-positive number is undefined." << endl;
+                    ans = 0;
+                } 
+                else {
+                    ans = log(num1);
+                }
+                break;
+            case 'S':
+                ans = pow(num1, 2);
+                break;
+            case 'C':
+                ans = pow(num1, 3);
+                break;
+            case 'p':
+                ans = pow(num1, num2);
+                break;
+            case 's':
+                ans = sin(num1);
+                break;
+            case 'c':
+                ans = cos(num1);
+                break;
+            case 't':
+                ans = tan(num1);
+                break;
+            case 'r':
+                if (num1 < 0) {
+                    cout << "Error: Square root of negative number is undefined." << endl;
+                    ans = 0;
+                } 
+                else {
+                    ans = sqrt(num1);
+                }
+                break;
+            case 'k':
+                converter = 0;
+                ans = 0;
+                break;
+            case '.':
+                cout << "Thank you for using our app.";
+                break;
+            default:
+                cout << "~~~~Invalid input!~~~~" << endl;
+        }
+    }
+    
+    return 0;
+}
+
+// Function for calculator interface
+void show_calc() {
+    cout << " ________________________________________________" << endl;
+    cout << "|---                                          ---|" << endl;
+    cout << "|-                  SCIENTIFIC                  -|" << endl;
+    cout << "|-                  CALCULATOR                  -|" << endl;
+    cout << "|---                                          ---|" << endl;
+    cout << "|------------------------------------------------|" << endl;
+    cout << "|-       +       |      -       |       *       -|" << endl;
+    cout << "|-      ADD      |    MINUS     |   MULTIPLY    -|" << endl;
+    cout << "|------------------------------------------------|" << endl;
+    cout << "|-       /       |      m       |       L       -|" << endl;
+    cout << "|-    DIVIDE     |     MOD      |      LOG      -|" << endl;
+    cout << "|------------------------------------------------|" << endl;
+    cout << "|-       S       |      C       |       p       -|" << endl;
+    cout << "|-    SQUARE     |     CUBE     |     POWER     -|" << endl;
+    cout << "|------------------------------------------------|" << endl;
+    cout << "|-       s       |      c       |       t       -|" << endl;
+    cout << "|-      SIN      |     COS      |      TAN      -|" << endl;
+    cout << "|------------------------------------------------|" << endl;
+    cout << "|-       r       |      k       |       .       -|" << endl;
+    cout << "|-    SQ ROOT    |    CLEAR     |     CLOSE     -|" << endl;
+    cout << "|________________________________________________|" << endl;
+}
+
+// Function for calculator output interface
+void show_output(double ans) {
+    cout << " ________________________________________________" << endl;
+    cout << "|---                   --------------------------|" << endl;
+    cout << "|-     CURRENT        |" << "                          |" << endl;
+    cout << "|-     OUTPUT IS:     |" << setw(25) << ans << " |" << endl;
+    cout << "|---                   --------------------------|" << endl;
+    cout << "|------------------------------------------------|" << endl;
+    cout << "|-       +       |      -       |       *       -|" << endl;
+    cout << "|-      ADD      |    MINUS     |   MULTIPLY    -|" << endl;
+    cout << "|------------------------------------------------|" << endl;
+    cout << "|-       /       |      m       |       L       -|" << endl;
+    cout << "|-    DIVIDE     |     MOD      |      LOG      -|" << endl;
+    cout << "|------------------------------------------------|" << endl;
+    cout << "|-       S       |      C       |       p       -|" << endl;
+    cout << "|-    SQUARE     |     CUBE     |     POWER     -|" << endl;
+    cout << "|------------------------------------------------|" << endl;
+    cout << "|-       s       |      c       |       t       -|" << endl;
+    cout << "|-      SIN      |     COS      |      TAN      -|" << endl;
+    cout << "|------------------------------------------------|" << endl;
+    cout << "|-       r       |      k       |       .       -|" << endl;
+    cout << "|-    SQ ROOT    |    CLEAR     |     CLOSE     -|" << endl;
+    cout << "|________________________________________________|" << endl;
+}
