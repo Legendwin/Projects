c85ec8364179c6b09a75bf144f34568101eb69dd
Legendwin <149319619+Legendwin@users.noreply.github.com>
2025-11-15 13:19:35 +0000

    fixed

--- Web Design/todo list/script.js
+++ Web Design/todo list/script.js
@@ -24,7 +24,7 @@ const clock = setInterval(function time() {
 
     // Updating the innerHTML of the day element to display the current date
     day.innerHTML = `${d} ${monthName[m]} ${y}`;
-    // Updating the textContent of the hour, minute, and second elements to display the current time
+
     hour.textContent = h;
     minute.innerText = min;
     second.innerText = sec;
@@ -40,60 +40,59 @@ const Todos = document.getElementById("todos");
 function addTodo() {
     // Checking if the input value is empty
     if (Input.value === '') {
-        alert("Please enter a todo!"); // Alerting the user to enter something
+        alert("Please enter a todo!");
     } else {
         // Creating a new list item (li) element for the todo
         let todo = document.createElement("li");
-        todo.id = 'todo-li'; // Setting the id for the new li element
-        todo.innerHTML = Input.value; // Setting the innerHTML of the li element to the input value
-        Todos.appendChild(todo); // Adding the new li element to the todos list
+        todo.id = 'todo-li'; 
+        todo.innerHTML = Input.value;
+        Todos.appendChild(todo);
 
-        // Creating a span element for the delete (×) button
         let span = document.createElement("span");
-        span.innerHTML = "\u00d7"; // Setting the innerHTML to ×
-        span.id = 'deleteBt'; // Setting the id for the delete button
-        todo.appendChild(span); // Adding the delete button to the todo item
+        span.innerHTML = "\u00d7";
+        span.id = 'deleteBt';
+        todo.appendChild(span);
 
-        // Creating a span element for the edit (✏) button
         let edit = document.createElement("span");
-        edit.innerHTML = "\u270f"; // Setting the innerHTML to ✏
-        edit.id = 'editBt'; // Setting the id for the edit button
-        todo.appendChild(edit); // Adding the edit button to the todo item
+        edit.innerHTML = "\u270f";
+        edit.id = 'editBt';
+        todo.appendChild(edit);
     }
-    Input.value = ''; // Clearing the input field
-    saveTodos(); // Saving the todos to local storage
+    Input.value = '';
+    saveTodos();
 }
 
 // Adding an event listener to the todos list for handling clicks
 Todos.addEventListener("click", function(e) {
-    // Checking if the clicked element is a list item
     if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked"); // Toggling the checked class
-        saveTodos(); // Saving the todos to local storage
-    // Checking if the clicked element is a delete button (×)
+        e.target.classList.toggle("checked");
+        saveTodos();
     } else if (e.target.tagName === "SPAN" && e.target.innerHTML === "\u00d7") {
-        e.target.parentElement.remove(); // Removing the todo item
-        saveTodos(); // Saving the todos to local storage
-    // Checking if the clicked element is an edit button (✏)
+        e.target.parentElement.remove(); 
+        saveTodos();
     } else if (e.target.tagName === "SPAN" && e.target.innerHTML === "\u270f") {
-        let todoLi = e.target.parentElement; // Getting the parent li element
-        // Setting the input value to the current todo text without the edit and delete buttons
+        let todoLi = e.target.parentElement;
         Input.value = todoLi.innerText.replace("\u270f", '').replace("\u00d7", '').trim();
-        todoLi.remove(); // Removing the todo item
-        saveTodos(); // Saving the todos to local storage
+        todoLi.remove();
+        saveTodos();
     }
-}, false); // Setting the useCapture flag to false
+}, false);
+
+document.getElementById("input").addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        addTodo();
+    }
+});
 
 // Function to save todos to local storage
 function saveTodos() {
-    localStorage.setItem("data", Todos.innerHTML); // Saving the innerHTML of the todos list to local storage
+    localStorage.setItem("data", Todos.innerHTML);
 }
 
 // Function to load todos from local storage
 function loadTodos() {
-    // Checking if there are todos saved in local storage
     if (localStorage.getItem("data")) {
-        Todos.innerHTML = localStorage.getItem("data"); // Setting the innerHTML of the todos list to the saved data
+        Todos.innerHTML = localStorage.getItem("data");
     }
 }
-loadTodos(); // Loading the todos when the script runs
+loadTodos();
